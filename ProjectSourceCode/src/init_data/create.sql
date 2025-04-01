bc03c555162d57eac4faeb7c331bebf6a7a8d3dd
Jeremy Huang <jeremylossi2016@gmail.com>
2025-04-01 13:30:00 -0600

    Revert "Merge pull request #30 from CoderLLamaPhone/create-fake-data"
    This reverts commit 462a6312ee1a8b9fec3948ae9ebcc7babb4ebce3, reversing
    changes made to c4188a27f915d6ea2321d34c3797a21ae3de6219.

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -9,12 +9,11 @@ DROP TABLE IF EXISTS driverInfo;
 CREATE TABLE driverInfo(
     driverID SERIAL PRIMARY KEY,
     username VARCHAR(60) NOT NULL,
-    AVG_Rating FLOAT(10)
+    AVG-Rating FLOAT(10),
 );
 
 DROP TABLE IF EXISTS driverRatings;
 CREATE TABLE driverRatings(
-    ratingID SERIAL PRIMARY KEY,
     driverID INTEGER NOT NULL,
     stars INTEGER NOT NULL,
     message VARCHAR(300) NOT NULL,
@@ -26,11 +25,11 @@ CREATE TABLE driverRatings(
 DROP TABLE IF EXISTS car;
 CREATE TABLE car(
     licensePlate VARCHAR(50) PRIMARY KEY,
-    ownerID INTEGER NOT NULL,
+    owner INTEGER NOT NULL,
     make VARCHAR(50) NOT NULL,
     model VARCHAR(50) NOT NULL,
     color VARCHAR(50) NOT NULL,
-    carType VARCHAR(50) NOT NULL,
+    type VARCHAR(50) NOT NULL,
     drivetrain VARCHAR(50) NOT NULL
 );
 
@@ -38,12 +37,11 @@ DROP TABLE IF EXISTS riderInfo;
 CREATE TABLE riderInfo(
     riderID SERIAL PRIMARY KEY,
     username VARCHAR(60) NOT NULL,
-    AVG_Rating FLOAT(10)
+    AVG-Rating FLOAT(10),
 );
 
 DROP TABLE IF EXISTS riderRatings;
 CREATE TABLE riderRatings(
-    ratingID SERIAL PRIMARY KEY,
     riderID INTEGER NOT NULL,
     stars INTEGER NOT NULL,
     message VARCHAR(300) NOT NULL,
@@ -64,12 +62,12 @@ CREATE TABLE trips(
     driverID INTEGER NOT NULL,
     capacity INTEGER NOT NULL,
     resort VARCHAR(50) NOT NULL,
-    EST_outbound TIME,
-    EST_return TIME,
+    EST-outbound TIME,
+    EST-return TIME,
     cost INTEGER NOT NULL,
     pickupLocation VARCHAR(50) NOT NULL,
     date DATE NOT NULL,
-    car VARCHAR(50) NOT NULL
+    car VARCHAR(50) NOT NULL,
 );
 
 DROP TABLE IF EXISTS chatroom;
@@ -88,10 +86,9 @@ CREATE TABLE resort(
 
 DROP TABLE IF EXISTS message;
 CREATE TABLE message(
-    messageID SERIAL PRIMARY KEY,
     chatroomID INTEGER NOT NULL,
     date DATE NOT NULL,
     time TIME NOT NULL,
-    username VARCHAR(50) NOT NULL,
+    user VARCHAR(50) NOT NULL,
     message VARCHAR(500) NOT NULL
 );
