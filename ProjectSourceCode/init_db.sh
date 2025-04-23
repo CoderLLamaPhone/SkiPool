447a88169d1bc42ac45b8e3255aec07c5df8ede9
CoderLLamaPhone <157067336+CoderLLamaPhone@users.noreply.github.com>
2025-04-23 19:33:19 +0000

    Removed extra files from PR

--- ProjectSourceCode/init_db.sh
+++
@@ -1,6 +0,0 @@
-PG_URI="postgresql://skipool_db_user:8JIZLRnWhmG4Prha2RNekFiW4GcBKTZm@dpg-d000qo2li9vc739eh0dg-a.oregon-postgres.render.com/skipool_db"
-# Execute each .sql file in the directory
-for file in src/init_data/*.sql; do
-    echo "Executing $file..."
-    psql $PG_URI -f "$file"
-done
